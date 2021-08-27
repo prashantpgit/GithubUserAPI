0c2d34889ab7a5b46cc294016528ae64b8e54156
prashantpgit <prashantpgit@gmail.com>
2021-08-27 19:52:03 +0530

    GithubUserAPI initial commit

---
+++ searchuser.js
@@ -0,0 +1,25 @@
+var form = document.getElementById("myForm");
+
+form.addEventListener('submit', function(e){
+  e.preventDefault();
+
+  var search = document.getElementById("search").value;
+
+  var realName = search.split(' ').join('');
+
+  fetch("https://api.github.com/users/" + realName)
+  .then((result) => result.json())
+  .then((data) => {
+
+    document.getElementById("result").innerHTML=`
+    <a target="_blank" href="https://www.github.com/${realName}" class="link-danger">
+	<img src="${data.avatar_url}"/><br>
+	<br>View Github Profile
+	</a><br/><br/>
+    <p>Name : ${data.name}</p>
+	<p>Bio : ${data.bio}</p>
+	<p>Followers : ${data.followers}</p>
+	<p>Following : ${data.following}</p>
+    `
+  })
+});
